cd874e75ca388743ad91bfb85926db407eb7de19
llggtty <junhuang.ann@gmail.com>
2019-01-26 14:53:02 +0800

    Update the_most_frequent.py

--- the_most_frequent.py
+++ the_most_frequent.py
@@ -9,16 +9,19 @@ Created on Sat Jan 26 00:58:37 2019
 """
 determine the most frequent strings in the sequence
 """
+import collections
+
 sequence = ['a','b','c','a','b','a']
 sequence = ['a','a','bi','bi','bi']
 
 
+#slow solution -> N^2 
+#def most_frequent(sequence):
+#    ls_count = [sequence.count(item) for item in sequence]
+#    return sequence[ls_count.index(max(ls_count))]
+
 
 def most_frequent(sequence):
-    ls_count = [sequence.count(item) for item in sequence]
-    return sequence[ls_count.index(max(ls_count))]
-
-
-
+    return collections.Counter(sequence).most_common(1)[0][0]
 
 most_frequent(sequence)        
